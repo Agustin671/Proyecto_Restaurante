9c1e1a55dd1f956e56badf8c4e25171156dd4df7
Agustin671 <agustincanas1@gmail.com>
2026-06-17 09:36:10 -0400

    Update database.js

--- Back/src/config/database.js
+++ Back/src/config/database.js
@@ -6,5 +6,16 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'mysql',
     logging: false
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
   }
 };
